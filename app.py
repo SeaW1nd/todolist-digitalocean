b83a70fc2505f230438812d19e24a699e59a65a3
SeaW1nd <vodanhlax@gmail.com>
2024-05-09 00:26:22 +0700

    Deploy online to digital ocean

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from api.main import create_app, socketio
 app = create_app()
 
 if __name__ == '__main__':
-    # app.run(debug=True, ssl_context='adhoc')
-    socketio.run(app, debug=True)
+    app.run(debug=True, ssl_context='adhoc')
+    # socketio.run(app, debug=True)
 
 
     
