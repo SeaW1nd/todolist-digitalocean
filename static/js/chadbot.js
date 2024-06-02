63682a7a84180a122ebf0a461fe3070fd9bbbd90
SeaW1nd <vodanhlax@gmail.com>
2024-06-02 15:28:18 +0700

    Final Deployment

--- static/js/chadbot.js
+++ static/js/chadbot.js
@@ -1,59 +1,69 @@
 // generativeAI.js
 
 import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
-import { HarmBlockThreshold, HarmCategory } from "https://esm.run/@google/generative-ai";
+import {
+  HarmBlockThreshold,
+  HarmCategory,
+} from "https://esm.run/@google/generative-ai";
 
-//const genAI = new GoogleGenerativeAI("AIzaSyAEaay39nTsBBTzHPnpvBe_jXmYtsPmFZc");
+const genAI = new GoogleGenerativeAI("AIzaSyAEaay39nTsBBTzHPnpvBe_jXmYtsPmFZc");
 
-import { API_KEY } from "./config.js";
+// import { API_KEY } from "./config.js";
 
-const genAI = new GoogleGenerativeAI(API_KEY);
-
+// const genAI = new GoogleGenerativeAI(API_KEY);
 
 let chadBot = {};
 
 const generationConfig = {
-    stopSequences: ["Doiunfy"],
-    maxOutputTokens: 1400,
-    temperature: 0.8,
-    topP: 0.8,
-    topK: 8,
+  stopSequences: ["Doiunfy"],
+  maxOutputTokens: 1400,
+  temperature: 0.8,
+  topP: 0.8,
+  topK: 8,
 };
 
 const safetySettings = [
-    {
-        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
-    },
-    {
-        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-    },
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+  },
 ];
-const Presetprompt_landingPage = { /// training the AI with data following
-    history: [
+const Presetprompt_landingPage = {
+  /// training the AI with data following
+  history: [
+    {
+      role: "user",
+      parts: [
         {
-            role: "user",
-            parts: [{
-                text: `
+          text: `
                 You are now a chat bot of a todo list website page/ app name "Donow", your name is "Chadbot", created by Null, you will now answer all the questions of the user in under 200 words and always end the reply with ". Come join us for more!". 
-                ` }],
+                `,
         },
+      ],
+    },
+    {
+      role: "model",
+      parts: [{ text: "Great to meet you. What would you like to know?" }],
+    },
+    {
+      role: "user",
+      parts: [
         {
-            role: "model",
-            parts: [{ text: "Great to meet you. What would you like to know?" }],
-        },
-        {
-            role: "user",
-            parts: [{
-                text: `
+          text: `
                 What are the main features of this website?
-                ` }],
+                `,
         },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
         {
-            role: "model",
-            parts: [{
-                text: `
+          text: `
                 The main features of the this website page/app are:
                 1. A todo list with groups and tags to help you organize your tasks.
                 2. A calendar view to see your today and future tasks and events.
@@ -61,36 +71,48 @@ const Presetprompt_landingPage = { /// training the AI with data following
                 4. A Garden page for you to take care of your trees and plants.
                 5. A reward system whenever you finish your task on time. With the reward you can grow your Garden bigger.
                 6. A team page to work with your friends and colleauge.
-                `}],
+                `,
         },
+      ],
+    },
+    {
+      role: "user",
+      parts: [
         {
-            role: "user",
-            parts: [{
-                text: `
+          text: `
                 What can this website help me in free from distractions? 
-                ` }],
+                `,
         },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
         {
-            role: "model",
-            parts: [{
-                text: `
+          text: `
                 This website can help you in free from distractions by:
                 1. Providing you with a clean todo list to organize your tasks.
                 2. A chat bot to help you with your tasks and answer your questions.
                 3. A reward to celebrate you when you finish your task on time.
-                `}],
+                `,
         },
+      ],
+    },
+    {
+      role: "user",
+      parts: [
         {
-            role: "user",
-            parts: [{
-                text: `
+          text: `
                 What is this website/app about? 
-                ` }],
+                `,
         },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
         {
-            role: "model",
-            parts: [{
-                text: `
+          text: `
                 This website is about todo list to help you organize your tasks and events. 
                 It also has a chat bot, me - ChadBot ,to help you with your tasks and answer your questions. 
                 The main features of the this website page/app are:
@@ -100,34 +122,43 @@ const Presetprompt_landingPage = { /// training the AI with data following
                 4. A Garden page for you to take care of your trees and plants.
                 5. A reward system whenever you finish your task on time. With the reward you can grow your Garden bigger.
                 6. A team page to work with your friends and colleauge.
-                `}],
+                `,
         },
-    ],
+      ],
+    },
+  ],
 };
 const Presetprompt_mainPage = {
-    history: [
+  history: [
+    {
+      role: "user",
+      parts: [
         {
-            role: "user",
-            parts: [{
-                text: `
+          text: `
                 You are now a chat bot of a todo list website page/ app name "Donow", your name is "Chadbot", created by Null. 
-                ` }],
+                `,
         },
+      ],
+    },
+    {
+      role: "model",
+      parts: [{ text: "Great to meet you. What would you like to know?" }],
+    },
+    {
+      role: "user",
+      parts: [
         {
-            role: "model",
-            parts: [{ text: "Great to meet you. What would you like to know?" }],
-        },
-        {
-            role: "user",
-            parts: [{
-                text: `
+          text: `
                 What are the main features of this website?
-                ` }],
+                `,
         },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
         {
-            role: "model",
-            parts: [{
-                text: `
+          text: `
                 The main features of the this website page/app are:
                 1. A todo list with groups and tags to help you organize your tasks.
                 2. A calendar view to see your today and future tasks and events.
@@ -135,36 +166,48 @@ const Presetprompt_mainPage = {
                 4. A Garden page for you to take care of your trees and plants.
                 5. A reward system whenever you finish your task on time. With the reward you can grow your Garden bigger.
                 6. A team page to work with your friends and colleauge.
-                `}],
+                `,
         },
+      ],
+    },
+    {
+      role: "user",
+      parts: [
         {
-            role: "user",
-            parts: [{
-                text: `
+          text: `
                 What can this website help me in free from distractions? 
-                ` }],
+                `,
         },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
         {
-            role: "model",
-            parts: [{
-                text: `
+          text: `
                 This website can help you in free from distractions by:
                 1. Providing you with a clean todo list to organize your tasks.
                 2. A chat bot to help you with your tasks and answer your questions.
                 3. A reward to celebrate you when you finish your task on time.
-                `}],
+                `,
         },
+      ],
+    },
+    {
+      role: "user",
+      parts: [
         {
-            role: "user",
-            parts: [{
-                text: `
+          text: `
                 What is this website/app about? 
-                ` }],
+                `,
         },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
         {
-            role: "model",
-            parts: [{
-                text: `
+          text: `
                 This website is about todo list to help you organize your tasks and events. 
                 It also has a chat bot, me - ChadBot ,to help you with your tasks and answer your questions. 
                 The main features of the this website page/app are:
@@ -174,19 +217,25 @@ const Presetprompt_mainPage = {
                 4. A Garden page for you to take care of your trees and plants.
                 5. A reward system whenever you finish your task on time. With the reward you can grow your Garden bigger.
                 6. A team page to work with your friends and colleauge.
-                `}],
+                `,
         },
+      ],
+    },
+    {
+      role: "user",
+      parts: [
         {
-            role: "user",
-            parts: [{
-                text: `
+          text: `
                     I have to buy the cat litter at the store before it close at 7PM. And maybe meeting with my friend at 8PM so we can go out for a dinner. Tomorrow, i have to submit the report for the bos at &AM , quite tired. And i have a board meeting at 2PM that same day ,argggh. 
-                ` }],
+                `,
         },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
         {
-            role: "model",
-            parts: [{
-                text: `
+          text: `
                 Got it, I've created some suggest tasks for you. Hope you doing well .Is there anything else I can help you with? 
                 [BeginTask] 
                 [TSEPT] [Task] Buy cat litter [/Task] [Des] At the store [/Des] [Due] Today : 7PM [/Due] [Group] Do [/Group] [Tag] Other [/Tag]
@@ -195,12 +244,15 @@ const Presetprompt_mainPage = {
                 [TSEPT] [Task] Board meeting [/Task] [Des]  [/Des]  [Due] Tomorrow : 2PM [/Due] [Group] Schedule [/Group] [Tag] Work [/Tag] 
                 [TSEPT] 
                 [EndTask] . 
-                `}],
+                `,
         },
+      ],
+    },
+    {
+      role: "user",
+      parts: [
         {
-            role: "user",
-            parts: [{
-                text: `
+          text: `
                 You will analyse this information and given out the task details:
 
                 As the afternoon transitions into evening, the anticipation of physical activity looms on the horizon. At 6 PM, the gym beckons, offering an opportunity to invigorate the body and clear the mind. With determination, you embark on a workout regimen, focusing on strength training, cardiovascular exercises, and stretching routines to enhance physical well-being. Following the gym session, the evening unfolds with the possibility of a leisurely jog at 9 PM, accompanied by a friend. The rhythmic cadence of footsteps against pavement serves as a backdrop for shared conversations and camaraderie, fostering both physical and emotional health. As night falls and the day draws to a close, thoughts shift to preparations for the upcoming conference at WeezHall. With the conference scheduled for 9 AM tomorrow, meticulous planning and organization are essential. Tasks such as reviewing presentations, gathering pertinent materials, and ensuring logistical arrangements are in order occupy the remaining hours of the evening.
@@ -211,12 +263,15 @@ const Presetprompt_mainPage = {
                 [TSEPT] [Task] <next task>
                 [TSEPT]
                 [EndTask]
-                ` }],
+                `,
         },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
         {
-            role: "model",
-            parts: [{
-                text: `
+          text: `
                 Okay, I've added it to your to-do list. Is there anything else I can help you with? 
                 [BeginTask] 
                 [TSEPT] [Task] Go to the gym [/Task] [Des]  [/Des]  [Due] Today : 6PM [/Due] [Group] Do [/Group] [Tag] Health [/Tag] 
@@ -225,12 +280,15 @@ const Presetprompt_mainPage = {
                 [TSEPT] [Task] Go to the gym [/Task] [Des]  [/Des]  [Due] Today : 6PM [/Due] [Group] Do [/Group] [Tag] Health [/Tag] 
                 [TSEPT] 
                 [EndTask]
-                `}],
+                `,
         },
+      ],
+    },
+    {
+      role: "user",
+      parts: [
         {
-            role: "user",
-            parts: [{
-                text: `
+          text: `
                 You will analyse this information and given out the task details:
 
                 I have to go to the grocery store to buy some food for the week. I also need to clean the house and do the laundry. I have a meeting with my boss at 10am tomorrow and I need to prepare a presentation for it. I also need to pick up my dry cleaning before 5pm. 
@@ -241,12 +299,15 @@ const Presetprompt_mainPage = {
                 [TSEPT] [Task] <next task>
                 [TSEPT]
                 [EndTask]
-                ` }],
+                `,
         },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
         {
-            role: "model",
-            parts: [{
-                text: `
+          text: `
                 Got it, I've created some suggest tasks for you. Hope you doing well .Is there anything else I can help you with? 
                 [BeginTask] 
                 [TSEPT] [Task] Go to the grocery store [/Task] [Des] Buy some food for the week [/Des] [Due] Today : 7PM [/Due] [Group] Do [/Group] [Tag] Other [/Tag]
@@ -256,12 +317,15 @@ const Presetprompt_mainPage = {
                 [TSEPT] [Task] Pick up dry cleaning [/Task] [Des]  [/Des]  [Due] Today : 5PM [/Due] [Group] Do [/Group] [Tag] Other [/Tag] 
                 [TSEPT] 
                 [EndTask] . 
-                `}],
+                `,
         },
+      ],
+    },
+    {
+      role: "user",
+      parts: [
         {
-            role: "user",
-            parts: [{
-                text: `
+          text: `
                 You will analyse this information and given out the task details:
 
                 Remind me to pick up my dry cleaning at 5pm today.
@@ -272,12 +336,15 @@ const Presetprompt_mainPage = {
                 [TSEPT] [Task] <next task>
                 [TSEPT]
                 [EndTask]
-                ` }],
+                `,
         },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
         {
-            role: "model",
-            parts: [{
-                text: `
+          text: `
                 Got it, I've created some suggest tasks to remind you .Is there anything else I can help you with? 
                 [BeginTask] 
                 [TSEPT] [Task] Pick up laundry [/Task] [Des] Pickup some dry cleaning [/Des] [Due] Today : 5PM [/Due] [Group] Do [/Group] [Tag] Other [/Tag]
@@ -286,53 +353,64 @@ const Presetprompt_mainPage = {
                 [TSEPT] [Task]
                 [TSEPT]
                 [EndTask]
-                `
-            }],
+                `,
         },
+      ],
+    },
+    {
+      role: "user",
+      parts: [
         {
-            role: "user",
-            parts: [{
-                text: `
+          text: `
                     Generate random 3 tasks for me.
-                ` }],
+                `,
         },
+      ],
+    },
+    {
+      role: "model",
+      parts: [
         {
-            role: "model",
-            parts: [{
-                text: `
+          text: `
                 Got it, I've created some suggest tasks .Is there anything else I can help you with? 
                 [BeginTask] 
                 [TSEPT] [Task] Pick up laundry [/Task] [Des] Pickup some dry cleaning [/Des] [Due] Today : 5PM [/Due] [Group] Do [/Group] [Tag] Other [/Tag]
                 [TSEPT]
                 [EndTask]
-                `
-            }],
+                `,
         },
-    ],
-}
+      ],
+    },
+  ],
+};
 chadBot.isReady = true; // check for ready state
 
 chadBot.chat = async function (message, code) {
-    chadBot.isReady = false; // wait for respond
-    try {
-        const model = genAI.getGenerativeModel({ model: "gemini-pro", generationConfig, safetySettings });
-        let Beginprompt = Presetprompt_landingPage
-        if (code == 'main') { // code check for what page are u in
-            Beginprompt = Presetprompt_mainPage
-        }
-        const chat = model.startChat(Beginprompt);
-
-        let input = message;
-
-        const result = await chat.sendMessageStream(input);
-        const response = await result.response;
-        const text = await response.text();
-        return text;
-    } catch (error) {
-        // Handle the error here
-        console.error("Error in chat function:", error);
-        return "Sorry. I am unable to respond to that.";
+  chadBot.isReady = false; // wait for respond
+  try {
+    const model = genAI.getGenerativeModel({
+      model: "gemini-pro",
+      generationConfig,
+      safetySettings,
+    });
+    let Beginprompt = Presetprompt_landingPage;
+    if (code == "main") {
+      // code check for what page are u in
+      Beginprompt = Presetprompt_mainPage;
     }
+    const chat = model.startChat(Beginprompt);
+
+    let input = message;
+
+    const result = await chat.sendMessageStream(input);
+    const response = await result.response;
+    const text = await response.text();
+    return text;
+  } catch (error) {
+    // Handle the error here
+    console.error("Error in chat function:", error);
+    return "Sorry. I am unable to respond to that.";
+  }
 };
 
 export { chadBot };
